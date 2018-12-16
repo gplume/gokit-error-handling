dba7083dc9f3a67220aa79346fb69c27b4ef460b
G.Plume <plumedev@gmail.com>
2018-12-16 12:10:44 +0100

    logger

--- middle/notify.go
+++ middle/notify.go
@@ -12,9 +12,9 @@ import (
 func Notify(logger kitlog.Logger) Wrapper {
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			logger.Log("Notify( before )", fmt.Sprintf("%s", r.URL.Path))
+			logger.Log("Route:", fmt.Sprintf("%s", r.URL.Path))
 			defer func(begin time.Time) {
-				logger.Log("Notify( after )", time.Since(begin).Round(time.Nanosecond))
+				logger.Log("Route:", r.URL.Path, "after", time.Since(begin).Round(time.Nanosecond))
 			}(time.Now())
 			h.ServeHTTP(w, r)
 		})
