afc45385f37b8c268df0a669aa86a9830080e8c3
G.Plume <plumedev@gmail.com>
2019-01-01 18:58:45 +0100

    intrumenting

--- middle/http_instrumenting.go
+++ middle/http_instrumenting.go
@@ -1,11 +1,18 @@
 package middle
 
 import (
+	"net/http"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
 	kitprometheus "github.com/go-kit/kit/metrics/prometheus"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
 var (
+	httpSuccessRegex, _ = regexp.Compile("^2[0-9]{2}$")
 	// RequestCount ...
 	RequestCount *kitprometheus.Counter
 	// RequestLatency ...
@@ -61,3 +68,36 @@ func init() {
 // 	n = mw.Next.Count(s)
 // 	return
 // }
+
+// ResponseWriter wraps the http.ResponseWriter for adding
+// the https response status code
+type ResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+// WriteHeader writes the http response status code and capture it inside the writer
+func (lrw *ResponseWriter) WriteHeader(code int) {
+	lrw.statusCode = code
+	lrw.ResponseWriter.WriteHeader(code)
+}
+
+// NewResponseWriter implements the ResponseWriter interface and is used
+// for capturing the http response status code
+func NewResponseWriter(w http.ResponseWriter) *ResponseWriter {
+	return &ResponseWriter{w, http.StatusOK}
+}
+
+// Middleware wraps a http handler for counting requests call and measuring request latency
+func Middleware(componentName string, handlerName string, counter *kitprometheus.Counter, latency *kitprometheus.Histogram, next http.Handler) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		lrw := NewResponseWriter(w)
+		defer func(begin time.Time) {
+			success := httpSuccessRegex.MatchString(strconv.Itoa(lrw.statusCode))
+			counter.With("component", componentName, "handler", handlerName, "code", strconv.Itoa(lrw.statusCode), "method", strings.ToLower(r.Method), "success", strconv.FormatBool(success)).Add(1)
+			latency.With("component", componentName, "handler", handlerName, "success", strconv.FormatBool(success)).Observe(time.Since(begin).Seconds())
+		}(time.Now())
+
+		next.ServeHTTP(lrw, r)
+	})
+}
